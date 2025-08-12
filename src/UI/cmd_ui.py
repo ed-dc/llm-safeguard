557661c5e46189f29ca4f93a596a1744904e6796
eddc <ed6dc@proton.me>
2025-08-12 21:13:21 +0200

    start : adding the first file for the project

---
+++ src/UI/cmd_ui.py
@@ -0,0 +1,25 @@
+####################
+# The cmd Line interface, where the user will comunicate with the model
+# Part of the LLM safeguard project
+####################
+
+
+
+class CmdUI():
+    """
+    Main class for CMD line UI
+
+    with methods :
+        - setup : set up the interface and get ready to show lines
+        - warning : show error with specific message
+        - assisant_line : add a line for the assistant
+        - user_line : add user line
+    """
+
+
+    def __init__(self):
+
+
+
+
+
